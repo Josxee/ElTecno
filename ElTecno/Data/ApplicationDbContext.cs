fae98bd53ec7d48ad1d02943bf79d0b242398f72
Landon Jarred Patterson Foster <landonfn@gmail.com>
2025-07-21 20:14:27 -0600

    Crud componentes

---
+++ ElTecno/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ElTecno.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElTecno.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Component> Components { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
