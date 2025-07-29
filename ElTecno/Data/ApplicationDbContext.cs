ad5de4c14facfdb3291d56b7f0d2bcfe9c5d22f7
JoseAlva115 <jdalvarad@gmail.com>
2025-07-28 18:23:18 -0600

    Agregado del api y de un cshtml del dashboard
    se agregó un chstml del media dashboard para que se trabaje ahi, además de agregar el api, pero no se pudo agregar por migraciones o probar por la base de datos, para que lo agreguen ahi el que hizo la BD

--- ElTecno/Data/ApplicationDbContext.cs
+++ ElTecno/Data/ApplicationDbContext.cs
@@ -12,5 +12,26 @@ namespace ElTecno.Data
             : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Component>().HasData(
+                new Component
+                {
+                    Id = 1,
+                    Name = "Api Clima",
+                    Type = "clima",
+                    ApiUrl = "https://api.open-meteo.com/v1/forecast?latitude=9.93&longitude=-84.08&current=temperature_2m",
+                    RefreshInterval = 30,
+                    IsActive = true,
+                    Description = "Api del Clima",
+                    Size = "medium",         
+                    Position = "medium", 
+                    IsFavorite = false,
+                    ApiKey = "****"
+                }
+            );
+        }
     }
 }
